80de3ecc828e91b1ff8c2b92c5af44f24973f3f0
neo-kjb <anaszawahreh.dev@gmail.com>
2024-03-19 03:50:13 +0300

    Sharing posts by Emai

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "anaszawahreh.dev@gmail.com"
+EMAIL_HOST_PASSWORD = "zamz whvf rrev xhlx"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
