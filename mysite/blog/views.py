e1d7e75198d7cc6a59275ebf4a1dbf45d18373e0
neo-kjb <anaszawahreh.dev@gmail.com>
2024-03-20 04:44:27 +0300

    Recommend similar posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
-from .models import Post, Comment
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from .models import Post
+from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
 
 
@@ -38,10 +39,22 @@ def post_detail(request, year, month, day, post):
 
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
+
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
