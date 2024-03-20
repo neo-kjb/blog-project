713cf7bf0bfafb9b5b983d4cb0445d7bbeda41d6
neo-kjb <anaszawahreh.dev@gmail.com>
2024-03-20 07:41:47 +0300

    Configuring PostgreSQL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "django.contrib.sitemaps",
+    "django.contrib.postgres",
     "taggit",
     "blog",
 ]
@@ -79,8 +80,10 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog",
+        "USER": "blog",
+        "PASSWORD": "arz.1231233",
     }
 }
 
